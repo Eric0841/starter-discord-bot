b02e1351077049581eab618887e2584591a2f969
Eric <101233077+Eric0841@users.noreply.github.com>
2023-04-20 14:58:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,113 +1,46 @@
-
-// const { clientId, guildId, token, publicKey } = require('./config.json');
-require('dotenv').config()
-const APPLICATION_ID = process.env.APPLICATION_ID 
-const TOKEN = process.env.TOKEN 
-const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
-
-
-const axios = require('axios')
-const express = require('express');
-const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
-
-
+const express = require('express')
 const app = express();
-// app.use(bodyParser.json());
+const port = 3000
+const Discord = require('discord.js');
+const config = require('./config.json')
+const client = new Discord.Client();
+app.get('/', (req, res) => res.send('lol'))
 
-const discord_api = axios.create({
-  baseURL: 'https://discord.com/api/',
-  timeout: 3000,
-  headers: {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-	"Access-Control-Allow-Headers": "Authorization",
-	"Authorization": `Bot ${TOKEN}`
-  }
-});
+app.listen(port, () =>
+  console.log(`봇 로그인 성공!`)
+);
+const { MessageButton, MessageActionRow } = require('discord-buttons');
+require('discord-buttons')(client);
 
+let sug = ['1088411211244253234','',''];
 
+let link = "";
 
+client.on("message", (message) => {
+  if (message.content !== `${config.command}`) return;
+  //message.content.delete()
+  const embed = new Discord.MessageEmbed()
+    .setTitle("인증")
+    .setDescription("인증을 하시려면 밑에 버튼을 눌러주세요!")
+    .setColor('#2f3136')
+    .setFooter('개발자 : RCat')
 
-app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
-  const interaction = req.body;
+  let verify = new MessageButton()
+    .setURL("https://restorecord.com/verify/RCAT")
+    .setLabel("버튼")
+    .setStyle("LINK")
+    .setEmoji("✅")
+  message.channel.send({
+    button: verify,
+    embed: embed
+  });
+  message.delete()
+})
 
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    console.log(interaction.data.name)
-    if(interaction.data.name == 'yo'){
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `Yo ${interaction.member.user.username}!`,
-        },
-      });
-    }
-
-    if(interaction.data.name == 'dm'){
-      // https://discord.com/developers/docs/resources/user#create-dm
-      let c = (await discord_api.post(`/users/@me/channels`,{
-        recipient_id: interaction.member.user.id
-      })).data
-      try{
-        // https://discord.com/developers/docs/resources/channel#create-message
-        let res = await discord_api.post(`/channels/${c.id}/messages`,{
-          content:'Yo! I got your slash command. I am not able to respond to DMs just slash commands.',
-        })
-        console.log(res.data)
-      }catch(e){
-        console.log(e)
-      }
-
-      return res.send({
-        // https://discord.com/developers/docs/interactions/receiving-and-responding#responding-to-an-interaction
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data:{
-          content:'👍'
-        }
-      });
-    }
-  }
-
-});
-
-
-
-app.get('/register_commands', async (req,res) =>{
-  let slash_commands = [
-    {
-      "name": "yo",
-      "description": "replies with Yo!",
-      "options": []
-    },
-    {
-      "name": "dm",
-      "description": "sends user a DM",
-      "options": []
-    }
-  ]
-  try
-  {
-    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
-    let discord_response = await discord_api.put(
-      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
-      slash_commands
-    )
-    console.log(discord_response.data)
-    return res.send('commands have been registered')
-  }catch(e){
-    console.error(e.code)
-    console.error(e.response?.data)
-    return res.send(`${e.code} error from discord`)
-  }
+client.on("ready", () => {
+  client.user.setActivity("인증 관리자 | 인증")
 })
 
 
-app.get('/', async (req,res) =>{
-  return res.send('Follow documentation ')
-})
 
-
-app.listen(8999, () => {
-
-})
-
+client.login(process.env.TOKEN)
